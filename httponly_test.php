510ff3ed101f0c3e3606d71957bca64980ae4d2b
Aero <aero@localhost.localdomain>
2018-09-10 11:58:10 +0200

    otras pruebas en /httponly_test

--- httponly_test.php
+++ httponly_test.php
@@ -1,9 +1,10 @@
 <?php
+echo "Hello world!";
 $name = 'foo';
 $value = 'bar';
 $expirationTime = 0;    // Session cookie.
-$path = '/';
-$domain = 'localhost';
+$path = '/httponly_test';
+$domain = 'nemrod962.github.io';
 $isSecure = false;
 $isHttpOnly = false;
 setcookie($name, $value, $expirationTime, $path, $domain, $isSecure, $isHttpOnly);?>
